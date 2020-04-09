8cf059159d597c77b1649b3c8d91a1f359485dfa
Jakob Kastelic <kastelic.jakob@gmail.com>
2020-04-09 19:37:18 -0400

    minor changes

--- figs/matplotlib/polarize.py
+++ figs/matplotlib/polarize.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.patches import Circle
 
 matplotlib.rcParams['svg.fonttype'] = 'none'
 matplotlib.rcParams.update({'font.size': 2})
@@ -16,7 +17,7 @@ aspect = 2
 ds = 10
 
 # generate data
-y, x = np.mgrid[slice(-xlim,xlim,N), slice(-ylim,ylim,N)]
+y, x = np.mgrid[slice(-xlim,xlim+N,N), slice(-ylim,ylim+N,N)]
 r = np.sqrt((x/aspect)**2 + y**2)
 cos = y / r
 psi_s = (1/np.sqrt(4*np.pi)) * 2 * np.exp(-r)
@@ -34,18 +35,18 @@ for i in range(3):
     axx[i].set_aspect(aspect)
     axx[i].axis('off')
 
-axx[0].pcolormesh(x,y,scale*psi_s, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
+axx[0].pcolormesh(x+.5,y,scale*psi_s, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
 axx[0].contour(x, y, psi_s, levels=[-1,-lvl,lvl,1], colors="black", linewidths=lw)
 axx[0].text(0,6,"\$\ket{\\text{s}}\$", fontdict={'ha':'center'})
 axx[0].scatter(0, 0, s=ds, c='black', marker='.')
 
-axx[1].pcolormesh(x,y,scale*psi_p, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
+axx[1].pcolormesh(x+.5,y,scale*psi_p, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
 axx[1].contour(x, y, psi_p, levels=[-1,-lvl,lvl,1], colors="black", linewidths=lw)
 axx[1].text(0,10,"\$\ket{\\text{p}}\$", fontdict={'ha':'center'})
 axx[1].text(0,-12,"(b)", fontdict={'ha':'center'})
 axx[1].scatter(0, 0, s=ds, c='black', marker='.')
 
-axx[2].pcolormesh(x,y,scale*psi, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
+axx[2].pcolormesh(x+.5,y,scale*psi, alpha=al, cmap='seismic',vmin=-1,vmax=1, rasterized=True)
 axx[2].contour(x, y, psi, levels=[-1,-lvl,lvl], colors="black", linewidths=lw)
 axx[2].text(0,8,"\$\ket{\\text{s}}+\eta\ket{\\text{p}}\$", fontdict={'ha':'center'})
 axx[2].scatter(0, 0, s=ds, c='black', marker='.')
